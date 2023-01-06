e2ac011f705d8347a15c3362300d8b4d0dcb1480
Harshit Singh Mahara <101328232+HarshitMahara@users.noreply.github.com>
2023-01-06 21:48:16 +0530

    Add files via upload

---
+++ intersection operation on sets.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],b[100],c[100],n1,n2,n,k=0,i,j;
+
+    // taking input of set A
+
+
+    printf("Enter number of element of set A\n");
+    scanf("%d",&n1);
+    printf("Enter elements of set A\n");
+    for(i=0;i<n1;i++)
+      scanf("%d",&a[i]);
+
+    // taking input set B
+
+    printf("Enter number of element of set B\n");
+    scanf("%d",&n2);
+    printf("Enter elements of set B\n");
+    for( i=0;i<n2;i++)
+      scanf("%d",&b[i]);
+    // printing set A
+    printf("\n set A is :- {");
+    for(i=0; i<n1;i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("}\n");
+    // printing set B
+    printf("\n set B is :- {");
+    for(i=0; i<n2;i++)
+    {
+        printf("%d ", b[i]);
+    }
+    printf("}\n");
+
+    // Logic for intersection
+
+    for( i=0;i<n1;i++)
+    {
+         for(j=0;j<n2;j++)
+         {
+            if(a[i]==b[j])
+            {
+                c[k]=a[i];
+                k++;
+            }
+         }
+
+    }
+
+    // Printing the elements of intersection of set A and set B
+    printf("\nIntersection of set A and set B is:- {");
+    for(i=0;i<k;i++)
+    {   printf("%d ",c[i]);
+      }
+    printf("}\n");
+
+
+    return 0;
+}
+
